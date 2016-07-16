77640b06f2ee4b524385735e492d2d36ddcb91bb
adrianhunter <jgradrian@gmail.com>
2016-07-16 17:10:32 +0200

    alksjhdlkjahs

---
+++ app/packages/bootstrap/lib/at_signup_link.js
@@ -0,0 +1,31 @@
+// Simply 'inherites' helpers from AccountsTemplates
+Template.atSignupLink.helpers(AccountsTemplates.atSignupLinkHelpers);
+
+// Simply 'inherites' events from AccountsTemplates
+Template.atSignupLink.events({
+    'click #at-signUp':function(e) {
+        e.preventDefault();
+        Meteor.loginWithHome_Connect({
+            requestPermissions: [
+                'ControlAppliance',
+                'Control',
+                'Oven-Control',
+                'Washer-Control',
+                'Dryer-Control',
+                'Coffeemaker-Control',
+                'Dishwasher-Control',
+                'IdentifyAppliance',
+                'Monitor',
+                'Oven-Monitor',
+                'Dishwasher-Monitor',
+                'Washer-Monitor',
+                'Dryer-Monitor',
+                'CoffeeMaker-Monitor',
+                'MonitorAppliance'
+            ]
+        },function(e,r){
+
+            Meteor.loginWithPassword('foo@foo.de', 'asdasd')
+        })
+    }
+});
